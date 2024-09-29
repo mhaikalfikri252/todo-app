375fb4c32f961463b94b8ff74b1e744cdfd1def8
mhaikalfikri252 <mhaikalfikri252@gmail.com>
2024-09-29 19:28:06 +0700

    fixing

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,29 @@
+/* eslint-disable react/prop-types */
+import { useContext } from "react";
+import { TodoContext } from "../App";
+
 const TodoItem = ({ todo }) => {
+  const { toogleCompleted, deleteTodo } = useContext(TodoContext);
+
+  const getTodoTitleStyle = () => {
+    if (todo.completed == true) {
+      return { textDecoration: "line-through" };
+    } else {
+      return { textDecoration: "none" };
+    }
+  };
+
   return (
     <div style={styles.title}>
-      <p>{todo.title}</p>
+      <input
+        type="checkbox"
+        style={styles.checkbox}
+        onChange={() => toogleCompleted(todo.id)}
+      />
+      <p style={getTodoTitleStyle()}>{todo.title}</p>
+      <button style={styles.button} onClick={() => deleteTodo(todo.id)}>
+        X
+      </button>
     </div>
   );
 };
@@ -10,6 +32,25 @@ const styles = {
   title: {
     border: "2px solid #f4f4f4",
     fontSize: "24px",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "0 20px",
+  },
+  checkbox: {
+    marginRight: "10px",
+    width: "18px",
+    height: "18px",
+  },
+  button: {
+    backgroundColor: "#BB0000",
+    color: "#fff",
+    height: "30px",
+    width: "30px",
+    borderRadius: "100%",
+    border: "none",
+    cursor: "pointer",
+    fontSize: "16px",
   },
 };
 
